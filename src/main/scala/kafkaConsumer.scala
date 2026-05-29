7d1ba0b6e55325b6b10b6c454ce680f14cc981c6
daniel2825 <daniel2825@hotmail.com>
2026-05-29 15:34:09 -0500

    kafka concepts

---
+++ src/main/scala/kafkaConsumer.scala
@@ -0,0 +1,51 @@
+import org.apache.kafka.clients.consumer.*
+import java.util.{Collections, Properties}
+import scala.jdk.CollectionConverters.*
+
+@main def runConsumer(): Unit =
+
+  val props = new Properties()
+
+  props.put(
+    ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,
+    "localhost:9092"
+  )
+
+  props.put(
+    ConsumerConfig.GROUP_ID_CONFIG,
+    "scala-group"
+  )
+
+  props.put(
+    ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
+    "org.apache.kafka.common.serialization.StringDeserializer"
+  )
+
+  props.put(
+    ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
+    "org.apache.kafka.common.serialization.StringDeserializer"
+  )
+
+  props.put(
+    ConsumerConfig.AUTO_OFFSET_RESET_CONFIG,
+    "earliest"
+  )
+
+  val consumer =
+    new KafkaConsumer[String, String](props)
+
+  consumer.subscribe(
+    Collections.singletonList("test-topic")
+  )
+
+  println("Esperando mensajes...")
+
+  while true do
+
+    val records =
+      consumer.poll(
+        java.time.Duration.ofSeconds(1)
+      )
+
+    for record <- records.asScala do
+      println(s"Mensaje recibido: ${record.value()}")
---
+++ src/main/scala/kafkaProducer.scala
@@ -0,0 +1,36 @@
+import org.apache.kafka.clients.producer.*
+import java.util.Properties
+
+@main def runProducer(): Unit =
+
+  val props = new Properties()
+
+  props.put(
+    ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
+    "localhost:9092"
+  )
+
+  props.put(
+    ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
+    "org.apache.kafka.common.serialization.StringSerializer"
+  )
+
+  props.put(
+    ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
+    "org.apache.kafka.common.serialization.StringSerializer"
+  )
+
+  val producer =
+    new KafkaProducer[String, String](props)
+
+  val record =
+    new ProducerRecord[String, String](
+      "test-topic",
+      "mensaje desde scala 3"
+    )
+
+  producer.send(record)
+
+  println("Mensaje enviado")
+
+  producer.close()
